5ac7aeff3232fa71588da9cfcda7d630fff0fc8c
Marllon dos Santos Costa <marllon.s.costa@ericssoninovacao.com.br>
2019-05-22 17:38:44 -0300

    Add configuration using xml

---
+++ src/framework/aurora/db/tools/LeituraXML.java
@@ -0,0 +1,37 @@
+package framework.aurora.db.tools;
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+
+import framework.aurora.db.parameters.DataBaseConfigurationConnectionParameter;
+
+public class LeituraXML {
+	
+	
+	public static DataBaseConfigurationConnectionParameter returnConfurationXML(String file) throws Exception{
+		File fXmlFile = new File("arquivo.xml");
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse(fXmlFile);
+		
+		System.out.println(doc.getElementsByTagName("PASSWORD").item(0).getTextContent());
+		
+		String dataBase = doc.getElementsByTagName("DATABASE").item(0).getTextContent();
+		String host = doc.getElementsByTagName("HOST").item(0).getTextContent();
+		String port = doc.getElementsByTagName("PORT").item(0).getTextContent();
+		String dataBaseName = doc.getElementsByTagName("DATABASENAME").item(0).getTextContent();
+		String user = doc.getElementsByTagName("USER").item(0).getTextContent();
+		String password = doc.getElementsByTagName("PASSWORD").item(0).getTextContent();
+		
+		
+		DataBaseConfigurationConnectionParameter configuration = new DataBaseConfigurationConnectionParameter(dataBase, host, port, dataBaseName, user, password);
+		
+		return configuration;
+		
+	}
+
+}
